b1fc830b6f699a5ea48364bcffda6e79545f3283
Nathen Harvey <nharvey@chef.io>
2015-12-29 13:21:30 -0500

    MVP profile

---
+++ metadata.rb
@@ -0,0 +1,4 @@
+name 'nathen/tmp_compliance_profile'
+version '0.1.0'
+title '/tmp Compliance Profile'
+maintainer 'Nathen Harvey <nharvey@chef.io>'
---
+++ test/tmp.rb
@@ -0,0 +1,19 @@
+title '/tmp Compliance Profile'
+
+control 'tmp-1.0' do
+  impact 0.3
+  title 'A /tmp directory must exist'
+  desc 'A /tmp directory must exist'
+  describe file '/tmp' do
+    it { should be_dierctory }
+  end
+end
+
+control 'tmp-1.1' do
+  impact 0.3
+  title '/tmp directory is owned by the root user'
+  desc 'The /tmp directory must be owned by the root user'
+  describe file '/tmp' do
+    it { should be_owned_by 'root' }
+  end
+end
